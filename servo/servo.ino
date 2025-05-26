c1560d9aaa24dda9a8428359e276e945cd5482df
Luis Eduardo Ocegueda Cortes <luiseduardoocegueda@gmail.com>
2025-05-26 11:14:06 -0600

    uso de servo

---
+++ servo/servo.ino
@@ -0,0 +1,49 @@
+#include <Servo.h>
+
+Servo myServo;
+const int LED_PIN = 13;
+const int BTN_I_PIN = 8; //Aumentar
+const int BTN_D_PIN = 7; //Disminuir
+
+void setup() {
+  // put your setup code here, to run once:
+  //Inicializar puerto serie
+  Serial.begin(9600);
+  //Conexiones
+  myServo.attach(6);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BTN_I_PIN, INPUT_PULLUP);
+  pinMode(BTN_D_PIN, INPUT_PULLUP);
+  //Poner el servo a 90 grados
+  myServo.write(90);
+}
+
+int currentPosition = 90;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(BTN_I_PIN) == LOW){
+    delay(50);
+    if(currentPosition < 180){
+      digitalWrite(LED_PIN, LOW);
+      currentPosition++;
+      myServo.write(currentPosition);
+    }else{
+      digitalWrite(LED_PIN, HIGH);
+    }
+  }
+
+   if(digitalRead(BTN_D_PIN) == LOW){
+    delay(50);
+    if(currentPosition > 0){
+      digitalWrite(LED_PIN, LOW);
+      currentPosition--;
+      myServo.write(currentPosition);
+    }else{
+      digitalWrite(LED_PIN, HIGH);
+    }
+  }
+
+  Serial.print("SERVO => ");
+  Serial.println(currentPosition);
+}
